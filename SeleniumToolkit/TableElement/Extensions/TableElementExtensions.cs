a007e9f3bee5af104b73a6317bb92d4542e8af8a
kiefercuevas <kiefercg2014@gmail.com>
2021-05-12 12:38:23 -0400

    Add and extension to convert to datatable
    Create an EdgeDriver provider

---
+++ SeleniumToolkit/TableElement/Extensions/TableElementExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumToolkit.TableElements.Extensions
+{
+    public static class TableElementExtensions
+    {
+        /// <summary>
+        /// Creates a new datatable object with the current table element
+        /// </summary>
+        /// <returns>A datatable that represents the table element</returns>
+        public static DataTable CreateDataTable(this TableElement tableElement)
+        {
+            DataTable tb = new();
+
+            string[] headers = tableElement.GetHeaders().ToArray();
+            foreach (string column in headers)
+                tb.Columns.Add(column);
+
+            foreach (var row in tableElement.GetRows())
+            {
+                DataRow newRow = tb.NewRow();
+                foreach (string column in headers)
+                    newRow[column] = row[column].Text;
+
+                tb.Rows.Add(newRow);
+            }
+            return tb;
+        }
+    }
+}
---
+++ SeleniumToolkit/WebDriverProvider/EdgeDriverProvider.cs
@@ -0,0 +1,46 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumToolkit.WebDriverProviders
+{
+    public class EdgeDriverProvider : IWebDriverProvider
+    {
+        public int TimeOutInMinutes { get; private set; }
+        public IWebDriver Driver { get; private set; }
+        public EdgeDriverProvider(int timeoutInMinutes = 5)
+        {
+            TimeOutInMinutes = timeoutInMinutes;
+        }
+
+        public void CloseDriver()
+        {
+            Driver.Quit();
+            Driver = null;
+        }
+
+        public void ResetDriver()
+        {
+            Driver.Quit();
+            Driver.Dispose();
+            Driver = null;
+            Driver = GetDriverInstance();
+            Driver.Manage().Window.Maximize();
+        }
+
+        public void Open()
+        {
+            Driver = GetDriverInstance();
+        }
+
+        public IWebDriver GetDriverInstance()
+        {
+            string path = Environment.CurrentDirectory;
+            return new EdgeDriver(path, options: new EdgeOptions(), commandTimeout: TimeSpan.FromMinutes(TimeOutInMinutes));
+        }
+    }
+}
